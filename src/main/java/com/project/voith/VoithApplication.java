da5503dda71a205e41e7db30c433b9ba274567c2
Topofira <samtothelink@gmail.com>
2020-05-30 17:36:27 -0400

    Initial commit

---
+++ src/main/java/com/project/voith/VoithApplication.java
@@ -0,0 +1,13 @@
+package com.project.voith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoithApplication.class, args);
+	}
+
+}
